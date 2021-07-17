386d5de7d2b3aef823765c452173bbeed2f105ee
OksanaMosendz <bagheera03@gmail.com>
2021-07-17 18:08:43 +0300

    create fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './sass/main.scss';
+import { fetchCountries } from './js/fetchCountries.js';
+
+const input= document.getElementById('search-box');
 
 const DEBOUNCE_DELAY = 300;
+
+
+// export {input};
+
+input.addEventListener('input',() => fetchCountries(input.value));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,16 @@
-function fetchCountries(){
+// import {input} from '../index';
 
+function fetchCountries(name){
 
+  // console.log(input.value);
+fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+  .then((response)=>{
+    return response.json();
+  })
+  .then((date)=>{
+    console.log(date);
+  })
 }
+
 
 export { fetchCountries }
