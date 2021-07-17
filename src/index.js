d1b648dbbe29920b338f2a024560d9f4d934a5af
OksanaMosendz <bagheera03@gmail.com>
2021-07-18 02:30:33 +0300

    add template

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { fetchCountries } from './js/fetchCountries.js';
 import debounce from 'lodash.debounce';
 
 const input= document.getElementById('search-box');
+const countryList=document.querySelector('.country-list');
+const countryInfo=document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY =300;
 
